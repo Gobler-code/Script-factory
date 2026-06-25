db1d7715c4a353cebcc3ff0ccc4a976883e88044
Gobler-code <goblergoss@gmail.com>
2026-06-25 22:02:29 +0545

    some practice

--- practice.py
+++ practice.py
@@ -1,18 +1,4 @@
-from pydantic import BaseModel
-class Person:
-    def __init__(self, name ,age):
-        self.name = name
-        self.age = age
-
-class PersonModel(BaseModel):
-    name:str
-    age: int
-
-p = Person("claude" ,5)
-p2= Person("claude2" ,"three")
-p3 = PersonModel(name="claude", age=3)
-
-
-print(p.name)
-print(p2.name)
-print(p3)
+text = "Hello world this is a test"
+words = text.split()
+print(words)
+print(len(words))
