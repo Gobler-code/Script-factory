b304727a7f7e832a77818a545d03a4aba43340fb
Gobler-code <goblergoss@gmail.com>
2026-07-23 21:43:48 +0545

    add new route for the side bar

--- frontend/landing-page/src/App.jsx
+++ frontend/landing-page/src/App.jsx
@@ -1,7 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
 import ScriptFactory from "./components/ScriptFactory";
+import CaptionsPage from "./components/CaptionPage";
+import VoiceoverPage from "./components/VoiceoverPage";
 
 function App() {
-  return <ScriptFactory />;
+  return (
+    <BrowserRouter>
+      <div className="flex min-h-screen bg-[#0a0a09]">
+        <Sidebar />
+        <div className="flex-1">
+          <Routes>
+            <Route path="/" element={<ScriptFactory />} />
+            <Route path="/captions" element={<CaptionsPage />} />
+            <Route path="/voiceover" element={<VoiceoverPage />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
