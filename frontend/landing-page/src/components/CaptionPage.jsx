38a12fa0207119d3d055c097c7b4e6e133758df8
Gobler-code <goblergoss@gmail.com>
2026-07-23 21:41:00 +0545

    create caption tool

---
+++ frontend/landing-page/src/components/CaptionPage.jsx
@@ -0,0 +1,9 @@
+import CaptionGenerator from "./CaptionGenerator";
+
+export default function CaptionsPage() {
+  return (
+    <div className="min-h-screen text-[#f3f1ea] p-6">
+      <CaptionGenerator initialScript="" />
+    </div>
+  );
+}
