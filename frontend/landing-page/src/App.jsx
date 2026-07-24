2a064cd563d962419561cc2521fd6abd52376abe
Gobler-code <goblergoss@gmail.com>
2026-07-24 22:51:10 +0545

    feat: add HistoryProvider for shared script history

--- frontend/landing-page/src/App.jsx
+++ frontend/landing-page/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HistoryProvider } from "./context/HistoryContext";
 import Sidebar from "./components/Sidebar";
 import ScriptFactory from "./components/ScriptFactory";
 import CaptionsPage from "./components/CaptionPage";
@@ -7,16 +8,18 @@ import VoiceoverPage from "./components/VoiceoverPage";
 function App() {
   return (
     <BrowserRouter>
-      <div className="flex min-h-screen bg-[#0a0a09]">
-        <Sidebar />
-        <div className="flex-1">
-          <Routes>
-            <Route path="/" element={<ScriptFactory />} />
-            <Route path="/captions" element={<CaptionsPage />} />
-            <Route path="/voiceover" element={<VoiceoverPage />} />
-          </Routes>
+      <HistoryProvider>
+        <div className="flex min-h-screen bg-[#0a0a09]">
+          <Sidebar />
+          <div className="flex-1">
+            <Routes>
+              <Route path="/" element={<ScriptFactory />} />
+              <Route path="/captions" element={<CaptionsPage />} />
+              <Route path="/voiceover" element={<VoiceoverPage />} />
+            </Routes>
+          </div>
         </div>
-      </div>
+      </HistoryProvider>
     </BrowserRouter>
   );
 }
